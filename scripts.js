cb2a7b70adeb6e2eb98d2285ec23bf690ba2ef09
YoumnaElayedd <125590651+YoumnaElayedd@users.noreply.github.com>
2023-06-29 20:01:40 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/* ================typing animationn===============*/
+var typed = new Typed(".typing", {
+  strings: ["Web-Designer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
